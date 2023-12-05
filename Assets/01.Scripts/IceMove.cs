1b90774ea42705167cc3e51ef5f92177daf0f6e1
mingyo0125 <wjdalsry125@gmail.com>
2023-12-05 18:29:23 +0900

    줌

--- Assets/01.Scripts/IceMove.cs
+++ Assets/01.Scripts/IceMove.cs
@@ -1,3 +1,4 @@
+using Cinemachine;
 using DG.Tweening;
 using System;
 using System.Collections;
@@ -6,9 +7,13 @@ using UnityEngine;
 
 public class IceMove : MonoBehaviour
 {
+    CinemachineVirtualCamera _virtualCam;
     [SerializeField] Transform glacierPos;
     [SerializeField] GameObject hexagonPos;
     [SerializeField] float speed = 5f;
+    [SerializeField]
+    private float cameraZoomFOV;
+    private float originalFOV;
 
     bool isMoving => Vector3.Distance(glacierPos.position, hexagonPos.transform.position) > 0.1f;
 
@@ -20,6 +25,7 @@ public class IceMove : MonoBehaviour
     {
         hexagonPos = GameObject.Find("HexagonPos");
         _outline = GetComponent<Outline>();
+        _virtualCam = GameObject.Find("Virtual Camera").GetComponent<CinemachineVirtualCamera>();
     }
 
     private void Start()
@@ -28,6 +34,7 @@ public class IceMove : MonoBehaviour
         targetColor = new Color(startColor.r, startColor.g, startColor.b, 1f);
 
         dir = hexagonPos.transform.position - glacierPos.position;
+        originalFOV = _virtualCam.m_Lens.FieldOfView;
         WaveManager.Instance.OnPhaseStartEvent += GroundMove;
     }
 
@@ -49,12 +56,17 @@ public class IceMove : MonoBehaviour
             .OnComplete(() =>
             {
                 // Fade out
-                DOTween.To(() => _outline.OutlineColor, color => _outline.OutlineColor = color, startColor, 0.7f);
+                DOTween.To(() => _outline.OutlineColor, color => _outline.OutlineColor = color, startColor, 0.7f).OnComplete(() =>
+                {
+                    _virtualCam.LookAt = transform;
+                    DOTween.To(() => _virtualCam.m_Lens.FieldOfView, fov => _virtualCam.m_Lens.FieldOfView = fov,
+                        cameraZoomFOV, 0.7f);
+                });
             });
     }
 
-    private void Update()
+    private void OnEnable()
     {
-        
+        WaveManager.Instance.OnPhaseStartEvent -= GroundMove;
     }
 }
