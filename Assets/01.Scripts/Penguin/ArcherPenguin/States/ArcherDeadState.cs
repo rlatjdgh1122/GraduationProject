e441be3e695f2a61ccab4547b9c3aa164cee8437
Chwfi <98933635+chwfi@users.noreply.github.com>
2023-12-05 23:17:18 +0900

    Archer State 추가
    Archer State 추가

---
+++ Assets/01.Scripts/Penguin/ArcherPenguin/States/ArcherDeadState.cs
@@ -0,0 +1,29 @@
+public class ArcherDeadState : ArcherBaseState
+{
+    public ArcherDeadState(Penguin penguin, PenguinStateMachine<ArcherPenguinStateEnum> stateMachine, string animBoolName)
+        : base(penguin, stateMachine, animBoolName)
+    {
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+        _triggerCalled = true;
+        if (_penguin.Target != null)
+            _penguin.Target.FindNearestPenguin("Player");
+        _penguin.tag = "Untagged";
+        _penguin.enabled = false;
+        _penguin.CharController.enabled = false;
+        _penguin.NavAgent.enabled = false;
+    }
+
+    public override void UpdateState()
+    {
+        base.UpdateState();
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+    }
+}
