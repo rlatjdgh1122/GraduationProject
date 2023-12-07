c9f14642a303b86fa146e640098bcc0d20a61463
Chwfi <98933635+chwfi@users.noreply.github.com>
2023-12-08 08:59:00 +0900

    바닥 NavMeshSurface 버그 수정
    바닥 NavMeshSurface 버그 수정

--- Assets/01.Scripts/Entity/Enemy/EnemyPenguin/States/EnemyPenguinMustChaseState.cs
+++ Assets/01.Scripts/Entity/Enemy/EnemyPenguin/States/EnemyPenguinMustChaseState.cs
@@ -17,22 +17,25 @@ public class EnemyPenguinMustChaseState : EnemyPenguinBaseState
         base.Enter();
         _triggerCalled = true;
         _nearestPayer = _enemy.FindNearestPenguin("Player");
+        if (_nearestPayer != null)
+            _enemy.SetTarget(_nearestPayer.transform.position);
     }
 
     public override void UpdateState()
     {
         base.UpdateState();
 
-        if (_nearestPayer != null)
-            _enemy.SetTarget(_nearestPayer.transform.position);
-
-        if (_enemy.IsTargetPlayerInside)
+        if (_enemy.IsTargetPlayerInside && _enemy.Target != null)
             _stateMachine.ChangeState(EnemyPenguinStateEnum.Chase);
 
         if (_enemy.NavAgent.remainingDistance < 0.1f && !_enemy.NavAgent.pathPending)
         {
             _stateMachine.ChangeState(EnemyPenguinStateEnum.Move);
         }
+
+
+        if (_enemy.IsDead)
+            _stateMachine.ChangeState(EnemyPenguinStateEnum.Dead);
     }
 
     public override void Exit()
