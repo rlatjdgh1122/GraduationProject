df1894740da0e3a6017a796389aa349314a97738
mingyo0125 <wjdalsry125@gmail.com>
2024-02-05 23:51:35 +0900

    이론상

--- Assets/01.Scripts/Building/BaseBuilding.cs
+++ Assets/01.Scripts/Building/BaseBuilding.cs
@@ -4,8 +4,56 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-[RequireComponent(typeof(Health))]
+//[RequireComponent(typeof(Health))]
 public abstract class BaseBuilding : PoolableMono
 {
+    private bool isSelected;
+    private bool isPlaced;
+
+
+    [SerializeField]
+    private Material[] _materials;
+
+    private Grid _grid;
+    
+    private MeshRenderer _meshRenderer;
+
+    private Coroutine _followMousePositionCoroutine;
+    
+    private Vector3 _mousePos => Input.mousePosition;
+
+
+    private void Awake()
+    {
+        _meshRenderer = GetComponent<MeshRenderer>();
+        _grid = GetComponent<Grid>();
+    }
+
+    public void SetSelect()
+    {
+        isSelected = true;
+        _meshRenderer.material = _materials[1];
+
+        _followMousePositionCoroutine = StartCoroutine(FollowMousePosition());
+    }
+
+    private IEnumerator FollowMousePosition()
+    {
+        while (true)
+        {
+            Vector3Int gridPosition = _grid.WorldToCell(_mousePos);
+            transform.position = _grid.CellToWorld(gridPosition);
+
+            yield return 0.1f;
+        }
+    }
+
+    public void Deselect()
+    {
+        isSelected = false;
+        _meshRenderer.material = _materials[0];
+
+
+    }
 
 }
