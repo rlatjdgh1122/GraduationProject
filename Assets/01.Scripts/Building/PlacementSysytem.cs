5abc0eca10e17c7a280600207772e6ddbfc69313
mingyo0125 <wjdalsry125@gmail.com>
2024-02-07 00:44:03 +0900

    잘막힘

--- Assets/01.Scripts/Building/PlacementSysytem.cs
+++ Assets/01.Scripts/Building/PlacementSysytem.cs
@@ -8,6 +8,7 @@ public class PlacementSysytem : MonoBehaviour
     private LayerMask _groundLayer;
 
     private BaseBuilding _curBuilding;
+    private Ground _curGround;
 
     public void SelectBuilding(BaseBuilding building)
     {
@@ -24,20 +25,32 @@ public class PlacementSysytem : MonoBehaviour
             if (GameManager.Instance.TryRaycast(GameManager.Instance.RayPosition(),
                                                 out var hit, Mathf.Infinity, _groundLayer))
             {
-                Vector3 hitPos = new Vector3(hit.transform.position.x, hit.point.y, hit.transform.position.z);
-                Vector3Int gridPosition = _curBuilding.BuildingInfoCompo.Grid.WorldToCell(hitPos);
-                _curBuilding.transform.position = _curBuilding.BuildingInfoCompo.Grid.CellToWorld(gridPosition);
-            }
+                _curGround = hit.transform.GetComponent<Ground>();
+                //만약 미리 설치되어 있으면 
+                if (_curGround.IsPlacedBuilding)
+                {
+                    Debug.Log("이미 설치되어 있음");
+                    yield return null;
+                }
+                else
+                {
+                    Vector3 hitPos = new Vector3(hit.transform.position.x, hit.point.y, hit.transform.position.z);
+                    Vector3Int gridPosition = _curBuilding.BuildingInfoCompo.Grid.WorldToCell(hitPos);
+                    _curBuilding.transform.position = _curBuilding.BuildingInfoCompo.Grid.CellToWorld(gridPosition);
 
-            if (Input.GetMouseButtonDown(0) && _curBuilding.gameObject.activeInHierarchy)
-            {
-                _curBuilding.Placed();
-                yield break;
-            }
+                    if (Input.GetMouseButtonDown(0) && _curBuilding.gameObject.activeInHierarchy)
+                    {
+                        _curBuilding.Placed();
+                        _curBuilding.transform.SetParent(hit.transform);
+                        _curGround.PlacedBuilding();
 
+                        _curGround = null;
+                        _curBuilding = null;
+                        yield break;
+                    }
+                }
+            }
             yield return null;
         }
     }
-
-    
 }
