7dbdac5643c0d559eeed0d4c69447e31c9071bd8
Chwfi <98933635+chwfi@users.noreply.github.com>
2024-02-15 20:28:33 +0900

    Resource와 Resource UI
    Resource와 Resource UI

--- Assets/01.Scripts/UI/NormalCanvasUI/ResourceUI.cs
+++ Assets/01.Scripts/UI/NormalCanvasUI/ResourceUI.cs
@@ -11,33 +11,51 @@ public class ResourceUI : NormalUI
     private CanvasGroup _canvasGroup;
     private TextMeshProUGUI _resourceNameText;
     private TextMeshProUGUI _recieveCountText;
+    private TextMeshProUGUI _needWorkerCountText;
+    private TextMeshProUGUI _currentWorkerCountText;
     private Image _resourceIcon;
 
-    private ResourceObject _resource;
+    private ResourceObject _CurrentResource;
 
     public override void Awake()
     {
         _canvasGroup = GetComponent<CanvasGroup>();
         _resourceNameText = transform.Find("_ResourceName").GetComponent<TextMeshProUGUI>();
         _recieveCountText = transform.Find("_RecieveCount").GetComponent<TextMeshProUGUI>();
+        _needWorkerCountText = transform.Find("_NeedWorkerCount").GetComponent<TextMeshProUGUI>();
+        _currentWorkerCountText = transform.Find("_CurrentWorkerCount").GetComponent<TextMeshProUGUI>();
         _resourceIcon = transform.Find("_ResourceIcon").GetComponent<Image>();
     }
 
     public override void EnableUI(float time, object obj)
     {
-        _resource = obj as ResourceObject;
+        _CurrentResource = obj as ResourceObject;
         Setting();
         _canvasGroup.DOFade(1, time);
     }
 
     private void Setting()
     {
-        _resourceNameText.text = _resource.ResourceName;
-        _resourceIcon.sprite = _resource.ResourceImage;
-        _recieveCountText.text = _resource.ReceiveCountWhenCompleted.ToString();
+        _resourceNameText.text = _CurrentResource.ResourceName;
+        _resourceIcon.sprite = _CurrentResource.ResourceImage;
+        _recieveCountText.text = _CurrentResource.ReceiveCountWhenCompleted.ToString();
+        _needWorkerCountText.text = $"최소 일꾼 {_CurrentResource.NeedWorkerCount}마리 필요";
+        _currentWorkerCountText.text = _CurrentResource.CurrentWorkerCount.ToString();
     }
 
-    public void Disable()
+    public void IncreaseWorkerCount()
+    {
+        _CurrentResource.CurrentWorkerCount++;
+        _currentWorkerCountText.text = _CurrentResource.CurrentWorkerCount.ToString();
+    }
+
+    public void SendWorkers()
+    {
+        //보내는 로직
+        ClosePanel();
+    }
+
+    public void ClosePanel()
     {
         DisableUI(1, null);
     }
