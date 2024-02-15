c4403ba8eef91f73890cb705268876a03918412b
mingyo0125 <wjdalsry125@gmail.com>
2024-02-15 19:33:33 +0900

    아 드디어 메모리leak해결

--- Assets/01.Scripts/Building/InstallSysytem.cs
+++ Assets/01.Scripts/Building/InstallSysytem.cs
@@ -9,21 +9,27 @@ public class InstallSysytem : MonoBehaviour
     private LayerMask _groundLayer;
 
     private BaseBuilding _curSelectedBuilding;
-    private Ground _curGround;
 
     private Dictionary<int, Ground> _groundDic = new Dictionary<int, Ground>(); //캐싱용 딕셔너리
 
+    private bool _buildingPlacementInProgress = false;
+
     public void SelectBuilding(BaseBuilding building)
     {
-        building.SetSelected();
-        _curSelectedBuilding = building;
+        if (!_buildingPlacementInProgress)
+        {
+            _curSelectedBuilding = building;
+            building.SetSelected();
 
-        StartCoroutine(BuildingFollowMousePosition());
+            StartCoroutine(BuildingFollowMousePosition());
+        }
     }
 
     private IEnumerator BuildingFollowMousePosition()
     {
-        while (true)
+        _buildingPlacementInProgress = true;
+
+        while (_curSelectedBuilding != null)
         {
             if (GameManager.Instance.TryRaycast(GameManager.Instance.RayPosition(),
                                                 out var hit, Mathf.Infinity, _groundLayer))
@@ -33,43 +39,40 @@ public class InstallSysytem : MonoBehaviour
                     _groundDic.Add(hit.transform.gameObject.GetHashCode(), hit.transform.GetComponent<Ground>());
                 }
 
-                _curGround = _groundDic[hit.transform.gameObject.GetHashCode()];
+                Ground curGround = null;
+                curGround = _groundDic[hit.transform.gameObject.GetHashCode()];
 
-                //만약 미리 설치되어 있으면 
-                if (_curGround.IsInstalledBuilding)
+                // 만약 미리 설치되어 있으면 
+                if (curGround.IsInstalledBuilding)
                 {
                     Debug.Log("이미 설치되어 있음");
-                    _curGround.ShowInstallPossibility(false); // 설치 불가능하다고 나타냄
-                    yield return new WaitForSeconds(0.25f);
+                    curGround.ShowInstallPossibility(false); // 설치 불가능하다고 나타냄
+                    yield return null;
                 }
-                else
+                else if (!curGround.IsInstalledBuilding && _curSelectedBuilding != null)
                 {
-                    _curGround.ShowInstallPossibility(true); // 설치 가능하다고 나타냄
+                    curGround.ShowInstallPossibility(true); // 설치 가능하다고 나타냄
                     Vector3 hitPos = new Vector3(hit.transform.position.x, hit.point.y, hit.transform.position.z);
                     Vector3Int gridPosition = _curSelectedBuilding.BuildingInfoCompo.Grid.WorldToCell(hitPos);
                     _curSelectedBuilding.transform.position = _curSelectedBuilding.BuildingInfoCompo.Grid.CellToWorld(gridPosition); // 그리드로 이동
 
-                    if (Input.GetMouseButtonDown(0) && _curSelectedBuilding.gameObject.activeInHierarchy) // 한번 더 누르면 설치
+                    if (Input.GetMouseButtonDown(0)) // 한번 더 누르면 설치
                     {
                         _curSelectedBuilding.transform.SetParent(hit.transform);
-                        _curSelectedBuilding.transform.position = hitPos;
                         _curSelectedBuilding.Installed(); // 건물에 설치 처리
-                        _curGround.InstallBuilding(); // 땅에 설치 처리
+                        curGround.InstallBuilding(); // 땅에 설치 처리
 
-                        _curGround = null;
                         _curSelectedBuilding = null;
+
+                        _buildingPlacementInProgress = false;
                         yield break;
                     }
-
                 }
             }
+
             yield return null;
         }
-    }
 
-    private IEnumerator TempCorou()
-    {
-        yield return new WaitForSeconds(0.25f);
-        Debug.Log("Test");
+        _buildingPlacementInProgress = false;
     }
 }
--- Assets/01.Scripts/LJS/LegionUI.cs
+++ Assets/01.Scripts/LJS/LegionUI.cs
@@ -1,133 +1,133 @@
-using TMPro;
-using UnityEngine;
+//using TMPro;
+//using UnityEngine;
 
-enum SlotType
-{
-    Sword,
-    Arrow,
-    Shield
-}
+//enum SlotType
+//{
+//    Sword,
+//    Arrow,
+//    Shield
+//}
 
-public class LegionUI : MonoBehaviour
-{
-    [SerializeField] private RectTransform _legionInventory;
-    [SerializeField] private float _targetPos;
-    [SerializeField] private float _duration;
+//public class LegionUI : MonoBehaviour
+//{
+//    [SerializeField] private RectTransform _legionInventory;
+//    [SerializeField] private float _targetPos;
+//    [SerializeField] private float _duration;
 
-    [SerializeField] private GameObject[] _slot;
-    [SerializeField] private TextMeshProUGUI[] _cntText;
+//    [SerializeField] private GameObject[] _slot;
+//    [SerializeField] private TextMeshProUGUI[] _cntText;
 
-    private Vector3 _firstUIPos;
-    private bool _isMoveUI;
+//    private Vector3 _firstUIPos;
+//    private bool _isMoveUI;
 
-    private TestLegion _legionManager;
+//    private TestLegion _legionManager;
 
-    private void Awake()
-    {
-        _legionManager = TestLegion.Instance;
-        _firstUIPos = _legionInventory.position;
-    }
+//    private void Awake()
+//    {
+//        _legionManager = TestLegion.Instance;
+//        _firstUIPos = _legionInventory.position;
+//    }
 
-    private void Update()
-    {
-        #region 임시 움직임
-        if (Input.GetKeyDown(KeyCode.J))
-        {
-            MoveLegionUI();
-        }
-        #endregion
+//    private void Update()
+//    {
+//        #region 임시 움직임
+//        if (Input.GetKeyDown(KeyCode.J))
+//        {
+//            MoveLegionUI();
+//        }
+//        #endregion
 
-        HeroCntTexOutput();
+//        HeroCntTexOutput();
 
-        if (_isMoveUI)
-            WaveManager.Instance.CanTimer = false;
-        else
-            WaveManager.Instance.CanTimer = true;
-    }
+//        if (_isMoveUI)
+//            WaveManager.Instance.CanTimer = false;
+//        else
+//            WaveManager.Instance.CanTimer = true;
+//    }
 
-    public void HeroCntTexOutput()
-    {
-        for (int i = 0; i < _cntText.Length; i++)
-        {
-            _cntText[i].text = $"{_slot[i].transform.childCount}";
-        }
-    }
+//    public void HeroCntTexOutput()
+//    {
+//        for (int i = 0; i < _cntText.Length; i++)
+//        {
+//            _cntText[i].text = $"{_slot[i].transform.childCount}";
+//        }
+//    }
 
-    public void LegionUIDownUnitCnt()
-    {
-        foreach (var legion in TestLegion.Instance.LegionCnt)
-        {
-            foreach (Transform child in legion._LegionPannel)
-            {
-                if (string.Equals(child.name, $"ItemObject[{SlotType.Arrow.ToString()}](Clone)"))
-                    legion.SpawnArrowCnt++;
-                if (string.Equals(child.name, $"ItemObject[{SlotType.Sword.ToString()}](Clone)"))
-                    legion.SpawnSwordCnt++;
-                if (string.Equals(child.name, $"ItemObject[{SlotType.Shield.ToString()}](Clone)"))
-                    legion.SpawnShieldCnt++;
-            }
+//    public void LegionUIDownUnitCnt()
+//    {
+//        foreach (var legion in TestLegion.Instance.LegionCnt)
+//        {
+//            foreach (Transform child in legion._LegionPannel)
+//            {
+//                if (string.Equals(child.name, $"ItemObject[{SlotType.Arrow.ToString()}](Clone)"))
+//                    legion.SpawnArrowCnt++;
+//                if (string.Equals(child.name, $"ItemObject[{SlotType.Sword.ToString()}](Clone)"))
+//                    legion.SpawnSwordCnt++;
+//                if (string.Equals(child.name, $"ItemObject[{SlotType.Shield.ToString()}](Clone)"))
+//                    legion.SpawnShieldCnt++;
+//            }
 
-            legion.TotalShieldCnt += legion.SpawnShieldCnt;
-            legion.TotalSwordCnt += legion.SpawnSwordCnt;
-            legion.TotalArrowCnt += legion.SpawnArrowCnt;
-        }
-    }
+//            legion.TotalShieldCnt += legion.SpawnShieldCnt;
+//            legion.TotalSwordCnt += legion.SpawnSwordCnt;
+//            legion.TotalArrowCnt += legion.SpawnArrowCnt;
+//        }
+//    }
 
-    public void LegionUIDownUnitReset()
-    {
-        for(int i = 0; i < TestLegion.Instance.LegionCnt.Count; i++)
-        {
-            TestLegion.Instance.LegionCnt[i].SpawnArrowCnt = 0;
-            TestLegion.Instance.LegionCnt[i].SpawnSwordCnt = 0;
-            TestLegion.Instance.LegionCnt[i].SpawnShieldCnt = 0;
-        }
-    }
+//    public void LegionUIDownUnitReset()
+//    {
+//        for(int i = 0; i < TestLegion.Instance.LegionCnt.Count; i++)
+//        {
+//            TestLegion.Instance.LegionCnt[i].SpawnArrowCnt = 0;
+//            TestLegion.Instance.LegionCnt[i].SpawnSwordCnt = 0;
+//            TestLegion.Instance.LegionCnt[i].SpawnShieldCnt = 0;
+//        }
+//    }
 
 
-    #region 군단 UI 추가
-    public void AddUI()
-    {
-        InstantiateHeroes(SlotType.Sword);
-        InstantiateHeroes(SlotType.Arrow);
-        InstantiateHeroes(SlotType.Shield);
-    }
+//    #region 군단 UI 추가
+//    public void AddUI()
+//    {
+//        InstantiateHeroes(SlotType.Sword);
+//        InstantiateHeroes(SlotType.Arrow);
+//        InstantiateHeroes(SlotType.Shield);
+//    }
 
-    private void InstantiateHeroes(SlotType slotType)
-    {
-        int slotIndex = (int)slotType;
+//    private void InstantiateHeroes(SlotType slotType)
+//    {
+//        int slotIndex = (int)slotType;
 
-        for (int j = 0; j < _legionManager.LegionUIList[slotIndex].HeroCnt; j++)
-        {
-            GameObject uiObj = Instantiate(TestLegion.Instance.LegionUIList[slotIndex].SlotUIPrefab);
-            uiObj.transform.SetParent(_slot[slotIndex].transform);
-            uiObj.GetComponent<RectTransform>().anchoredPosition = Vector3.zero;
-        }
+//        for (int j = 0; j < _legionManager.LegionUIList[slotIndex].HeroCnt; j++)
+//        {
+//            GameObject uiObj = Instantiate(TestLegion.Instance.LegionUIList[slotIndex].SlotUIPrefab);
+//            uiObj.transform.SetParent(_slot[slotIndex].transform);
+//            uiObj.GetComponent<RectTransform>().anchoredPosition = Vector3.zero;
+//        }
 
-        TestLegion.Instance.LegionUIList[slotIndex].HeroCnt = 0;
-    }
-    #endregion
+//        TestLegion.Instance.LegionUIList[slotIndex].HeroCnt = 0;
+//    }
+//    #endregion
 
-    #region UI 움직임
-    public void MoveLegionUI()
-    {
-        //if (WaveManager.Instance.IsPhase) _isMoveUI = false;
-        //else 
-        _isMoveUI = !_isMoveUI;
-        Vector3 targetPos;
+//    #region UI 움직임
+//    public void MoveLegionUI()
+//    {
+//        //if (WaveManager.Instance.IsPhase) _isMoveUI = false;
+//        //else 
+//        _isMoveUI = !_isMoveUI;
+//        Vector3 targetPos;
 
-        if(_isMoveUI)
-        {
-            targetPos = new Vector3(_targetPos + _firstUIPos.x, _firstUIPos.y, 0);
-            AddUI(); //임시
-            LegionUIDownUnitReset(); //임시
-        }
-        else
-        {
-            targetPos = _firstUIPos;
-            LegionUIDownUnitCnt();
-        }
+//        if(_isMoveUI)
+//        {
+//            targetPos = new Vector3(_targetPos + _firstUIPos.x, _firstUIPos.y, 0);
+//            AddUI(); //임시
+//            LegionUIDownUnitReset(); //임시
+//        }
+//        else
+//        {
+//            targetPos = _firstUIPos;
+//            LegionUIDownUnitCnt();
+//        }
 
-        UIManager.Instance.UIMoveDot(_legionInventory, targetPos, _duration);
-    }
-    #endregion
-}
+//        UIManager.Instance.UIMoveDot(_legionInventory, targetPos, _duration);
+//    }
+//    #endregion
+//}
