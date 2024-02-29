23d9ba3d00558219047e92a60bd0448b24a5a148
Chwfi <98933635+chwfi@users.noreply.github.com>
2024-02-29 15:42:32 +0900

    GeneralShip 움직임, 궁수 체력SO 수정, 전투 시작 시 일하던 일꾼도 돌아가게 수정
    GeneralShip 움직임, 궁수 체력SO 수정, 전투 시작 시 일하던 일꾼도 돌아가게 수정

--- Assets/01.Scripts/Building/Buildings/DefaultBuilding/GeneralShip.cs
+++ Assets/01.Scripts/Building/Buildings/DefaultBuilding/GeneralShip.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -17,5 +18,11 @@ public class GeneralShip : MonoBehaviour
     {
         Debug.Log("ÀÀ´­·È¾î");
         _generalUI.EnableUI(0.5f, null);
+    }
+
+    private void Start()
+    {
+        transform.DOMoveY(-0.1f, 3.2f).SetLoops(-2, LoopType.Yoyo);
+        transform.DORotate(new Vector3(-1.2f, -60, 0f), 3.2f).SetLoops(-2, LoopType.Yoyo);
     }
 }
