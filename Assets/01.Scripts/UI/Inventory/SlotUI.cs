050b259e0f7de30efddbab103cad552ab88654cc
leewnstj <ij35492581@gmail.com>
2024-02-29 17:08:00 +0900

    선택 이미지
    선택 이미지

--- Assets/01.Scripts/UI/Inventory/SlotUI.cs
+++ Assets/01.Scripts/UI/Inventory/SlotUI.cs
@@ -8,6 +8,12 @@ public abstract class SlotUI : MonoBehaviour
     [SerializeField] protected Image _unitImage;
     [SerializeField] protected Sprite _emptyImage;
     public LegionInventoryData _data = null;
+    protected LegionInventoryUI _legionInventoryUI;
+
+    protected virtual void Awake()
+    {
+        _legionInventoryUI = transform.parent.parent.parent.parent.parent.GetComponent<LegionInventoryUI>();
+    }
 
     public abstract void UpdateSlot(LegionInventoryData data);
     public abstract void CleanUpSlot();
