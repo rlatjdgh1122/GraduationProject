14f2039c801b8daef8bc2c4ab39e5b7cc9acdef2
mingyo0125 <wjdalsry125@gmail.com>
2024-03-06 00:17:34 +0900

    아쳐타워기본

--- Assets/01.Scripts/Building/Buildings/DefenseBuilding/ArcherTowerBuilding.cs
+++ Assets/01.Scripts/Building/Buildings/DefenseBuilding/ArcherTowerBuilding.cs
@@ -4,7 +4,22 @@ using UnityEngine;
 
 public class ArcherTowerBuilding : BaseBuilding
 {
+    FieldOfView _fov;
+
+    HashSet<Transform> _visibleTargets => _fov.FindVisibleTargets();
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        _fov = GetComponent<FieldOfView>();
+    }
+
     protected override void Running()
     {
+        if(_visibleTargets.Count >= 0)
+        {
+            Debug.Log("발견을했다????");
+        }
     }
 }
