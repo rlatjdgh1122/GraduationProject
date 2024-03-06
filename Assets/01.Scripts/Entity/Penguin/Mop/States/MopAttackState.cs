18d30f7d1d4ea9e5b9b64c4cd3189153981fddcc
Ojunsun <98933734+Ojunsun@users.noreply.github.com>
2024-03-06 19:48:36 +0900

    Mop펭귄 스테이트 생성

---
+++ Assets/01.Scripts/Entity/Penguin/Mop/States/MopAttackState.cs
@@ -0,0 +1,38 @@
+public class MopAttackState : MopBaseState
+{
+    public MopAttackState(Penguin penguin, EntityStateMachine<MopPenguinStateEnum, Penguin> stateMachine, string animBoolName) 
+        : base(penguin, stateMachine, animBoolName)
+    {
+    }
+
+    public override void Enter() //한명이 때리다가 죽으면 
+    {
+        base.Enter();
+        _triggerCalled = false;
+        _penguin.FindFirstNearestEnemy();
+        _penguin.Owner.IsMoving = false;
+        _penguin.StopImmediately();
+        _penguin.AnimatorCompo.speed = _penguin.attackSpeed;
+    }
+
+    public override void UpdateState()
+    {
+        base.UpdateState();
+        _penguin.LookTarget();
+
+        if (_triggerCalled)
+        {
+            _stateMachine.ChangeState(MopPenguinStateEnum.Chase);
+
+            if (_penguin.CurrentTarget == null)
+                _stateMachine.ChangeState(MopPenguinStateEnum.Idle);
+        }
+    }
+
+    public override void Exit()
+    {
+        _penguin.AnimatorCompo.speed = 1;
+        _penguin.Owner.IsMoving = true;
+        base.Exit();
+    }
+}
---
+++ Assets/01.Scripts/Entity/Penguin/Mop/States/MopIdleState.cs
@@ -0,0 +1,29 @@
+public class MopIdleState : MopBaseState
+{
+    public MopIdleState(Penguin penguin, EntityStateMachine<MopPenguinStateEnum, Penguin> stateMachine, string animBoolName) 
+        : base(penguin, stateMachine, animBoolName)
+    {
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+        _triggerCalled = true;
+    }
+
+    public override void UpdateState()
+    {
+        base.UpdateState();
+
+        if (_penguin.NavAgent.velocity.magnitude > 0.05f)
+            _stateMachine.ChangeState(MopPenguinStateEnum.Move);
+
+        if (_penguin.IsInnerTargetRange)
+            _stateMachine.ChangeState(MopPenguinStateEnum.Chase);
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+    }
+}
