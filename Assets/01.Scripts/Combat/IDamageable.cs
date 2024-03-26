af162126bc43372a805723f26fe5c41ec25b1b00
leewnstj <ij35492581@gmail.com>
2024-03-26 18:20:42 +0900

    .
    .

--- Assets/01.Scripts/Combat/IDamageable.cs
+++ Assets/01.Scripts/Combat/IDamageable.cs
@@ -14,6 +14,7 @@ public enum HitType
     BleedHit,
     GroundStrikeHit,
     BearSlashHit,
+    BiteHit
 }
 
 public interface IDamageable
--- Assets/01.Scripts/Entity/Enemy/EnemyAnimals/Bear/BearAttackableEntity.cs
+++ Assets/01.Scripts/Entity/Enemy/EnemyAnimals/Bear/BearAttackableEntity.cs
@@ -1,11 +1,28 @@
+using StatOperator;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BearAttackableEntity : AnimalAttackableEntity
 {
+    private List<float> _savedKncbackValue;
+
+    [Header("Percentage")]
+    [Range(0,10)]   
+    [SerializeField] private int _knockbackIncreaseToHPPercent;
+    [Header("Slash Effect")]
     [SerializeField] private ParticleSystem _leftHandEffectTransform;
     [SerializeField] private ParticleSystem _rightHandEffectTransform;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        foreach(var i in animalAttack)
+        {
+            _savedKncbackValue.Add(i.KnbackValue);
+        }
+    }
 
     public void StartEffect()
     {
@@ -17,5 +34,22 @@ public class BearAttackableEntity : AnimalAttackableEntity
         {
             _leftHandEffectTransform.Play();
         }
+    }
+
+    public override void AoEAttack(bool Knb, float value)
+    {
+        float damageReceivedPercent = StatCalculator.ReturnPercent(owner.HealthCompo.maxHealth, owner.HealthCompo.currentHealth);
+
+        if(damageReceivedPercent >= 50)
+        {
+            animalAttack[ComboCounter].KnbackValue = _savedKncbackValue[ComboCounter];
+        }
+        else
+        {
+            animalAttack[ComboCounter].KnbackValue *= damageReceivedPercent / 100;
+        }
+        
+
+        base.AoEAttack(Knb, value);
     }
 }
