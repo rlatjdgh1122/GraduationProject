4638385ae9a089290c4ce64d28ccaa8974005088
Ojunsun <98933734+Ojunsun@users.noreply.github.com>
2024-04-04 14:33:46 +0900

    위자드 펭귄 에셋 적용 및 스테이트 작성
    위자드 펭귄 에셋 적용 및 스테이트 작성

---
+++ Assets/01.Scripts/Weapon/Meteor.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Meteor : PoolableMono
+{
+    [SerializeField] private float _bulletPower;
+
+    private Rigidbody _rigid;
+    private DamageCaster _damageCaster;
+
+    private void OnEnable()
+    {  
+        _rigid = GetComponent<Rigidbody>();
+        _damageCaster = GetComponent<DamageCaster>();
+        StartCoroutine(WaitForDestroy());
+    }
+
+    public void Setting(Entity owner, LayerMask layer)
+    {  
+        _damageCaster.SetOwner(owner);
+        _damageCaster.TargetLayer = layer; 
+    }
+
+    public void Fire(Vector3 dir)
+    {
+        _rigid.AddForce(dir * _bulletPower, ForceMode.Impulse);  
+    }
+
+    private IEnumerator WaitForDestroy()
+    {
+        yield return new WaitForSeconds(4);
+        Destroy(this.gameObject);
+    }
+
+    private void OnTriggerEnter(Collider coll)
+    {
+        if (_damageCaster.CastArrowDamage(coll, _damageCaster.TargetLayer))
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
