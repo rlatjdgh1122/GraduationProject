4da18f5317822a4963a826e77195ac3753302821
Ojunsun <98933734+Ojunsun@users.noreply.github.com>
2024-04-08 18:17:30 +0900

    wizard

--- Assets/01.Scripts/Weapon/Meteor.cs
+++ Assets/01.Scripts/Weapon/Meteor.cs
@@ -5,40 +5,45 @@ using UnityEngine;
 
 public class Meteor : PoolableMono
 {
-    [SerializeField] private float _bulletPower;
+    [SerializeField] private float _meteorPower;
+    private bool _canMove = false;
 
     private Rigidbody _rigid;
     private DamageCaster _damageCaster;
 
     private void OnEnable()
-    {  
+    {
         _rigid = GetComponent<Rigidbody>();
         _damageCaster = GetComponent<DamageCaster>();
-        StartCoroutine(WaitForDestroy());
     }
 
     public void Setting(Entity owner, LayerMask layer)
-    {  
+    {
         _damageCaster.SetOwner(owner);
-        _damageCaster.TargetLayer = layer; 
+        _damageCaster.TargetLayer = layer;
     }
 
     public void Fire(Vector3 dir)
     {
-        _rigid.AddForce(dir * _bulletPower, ForceMode.Impulse);  
-    }
+        StartCoroutine(BeforeMeteorAtk());
 
-    private IEnumerator WaitForDestroy()
-    {
-        yield return new WaitForSeconds(4);
-        Destroy(this.gameObject);
-    }
-
-    private void OnTriggerEnter(Collider coll)
-    {
-        if (_damageCaster.CastArrowDamage(coll, _damageCaster.TargetLayer))
+        if (_canMove)
         {
-            Destroy(this.gameObject);
+            _rigid.AddForce(dir * _meteorPower, ForceMode.Impulse);
         }
+    }
+
+    private IEnumerator BeforeMeteorAtk()
+    {
+        yield return new WaitForSeconds(3);
+        _canMove = true;
+    }
+
+    private void Explode()
+    {
+        // 메테오 폭발 로직 추가
+        _damageCaster.CastMeteorDamage(transform.position, _damageCaster.TargetLayer);
+        _canMove = false;
+        Destroy(this.gameObject);
     }
 }
