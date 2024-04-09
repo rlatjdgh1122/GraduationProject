ecb0378d9ab1d176bcb46b5c889182e833cc985b
leewnstj <ij35492581@gmail.com>
2024-04-09 18:49:19 +0900

    .
    .

--- Assets/01.Scripts/UI/LegionInventorySlot/LegionSlotUI.cs
+++ Assets/01.Scripts/UI/LegionInventorySlot/LegionSlotUI.cs
@@ -51,11 +51,8 @@ public abstract class LegionSlotUI : SlotUI
 
         if (legion.SelectData.InfoData.JobType == PenguinJobType.General && legion.LimitOfGeneral()) //장군이 이미 군단에 있고 장군을 슬롯에 추가할려고 하면
         {
-            if (legion.SelectData.SlotType != SlotType.Legion)
-            {
-                UIManager.Instance.ShowWarningUI("장군이 이미 군단에 포함되어 있습니다!");
-                return;
-            }
+            UIManager.Instance.ShowWarningUI("장군이 이미 군단에 포함되어 있습니다!");
+            return;
         }
         else if (legion.SelectData.InfoData.JobType != PenguinJobType.General && legion.ExcedLimitOfLegion()) //만약 군단 안에 펭귄이 최대 펭귄 수를 넘었으면
         {
