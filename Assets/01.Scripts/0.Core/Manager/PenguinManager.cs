12b4d13537dc804963778c743fef42c68abbebd3
rlatjdgh1122 <98933564+rlatjdgh1122@users.noreply.github.com>
2024-04-12 14:28:58 +0900

    정보창 띄우는중

--- Assets/01.Scripts/0.Core/Manager/PenguinManager.cs
+++ Assets/01.Scripts/0.Core/Manager/PenguinManager.cs
@@ -311,7 +311,6 @@ public class PenguinManager
                 NotBelongDummyPenguinList.Add(item.dummyPenguin);
             }
         }
-
     }
 }
 
--- Assets/01.Scripts/UI/InfoUI/PenguinInfoUI.cs
+++ Assets/01.Scripts/UI/InfoUI/PenguinInfoUI.cs
@@ -2,17 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PenguinInfoUI : MonoBehaviour
+public void 
+
+public class PenguinInfoUI : PopupUI
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public override void HidePanel()
     {
-        
+        base.HidePanel();
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void ShowPanel()
     {
-        
+        base.ShowPanel();
     }
 }
