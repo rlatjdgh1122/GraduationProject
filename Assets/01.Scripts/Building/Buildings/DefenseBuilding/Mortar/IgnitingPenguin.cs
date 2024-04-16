0ac02943483d531607b5a97064b0b3e6a1692802
mingyo0125 <wjdalsry125@gmail.com>
2024-04-16 19:19:37 +0900

    박격포 심지 타들어 가는 것

--- Assets/01.Scripts/Building/Buildings/DefenseBuilding/Mortar/IgnitingPenguin.cs
+++ Assets/01.Scripts/Building/Buildings/DefenseBuilding/Mortar/IgnitingPenguin.cs
@@ -6,9 +6,18 @@ public class IgnitingPenguin : MonoBehaviour
 {
     Animator _animator;
 
+    [SerializeField]
+    private GameObject _torch;
+
+    private Material _ropeMat;
+
+    private readonly float minRopeMatcutoffValue = 4.35f;
+    private readonly float maxRopeMatcutoffValue = 4.75f;
+
     private void Awake()
     {
-        _animator = transform.Find("Visual").GetComponent<Animator>();
+        _animator = GetComponent<Animator>();
+        _ropeMat = transform.root.Find("rope").GetComponent<MeshRenderer>().material;
     }
 
     private void Update()
@@ -22,5 +31,30 @@ public class IgnitingPenguin : MonoBehaviour
     public void SetGetTourchAnimation()
     {
         _animator.SetTrigger("GetTorchTrigger");
+    }
+
+    public void GetTorch()
+    {
+        _torch.SetActive(true);
+    }
+
+    public void UseTorch()
+    {
+        _torch.SetActive(true);
+        StartCoroutine(SetRopeBurningMat());
+    }
+
+    private IEnumerator SetRopeBurningMat()
+    {
+        float startTime = Time.time;
+
+        while (Time.time - startTime < 3f)
+        {
+            float t = (Time.time - startTime) / 3f;
+            _ropeMat.SetFloat("_CutoffValue", Mathf.Lerp(maxRopeMatcutoffValue, minRopeMatcutoffValue, t));
+            yield return new WaitForEndOfFrame();
+        }
+
+        _ropeMat.SetFloat("_CutoffValue", minRopeMatcutoffValue);
     }
 }
