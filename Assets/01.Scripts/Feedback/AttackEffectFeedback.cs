3268dd4c11cddefd37169525e0dc8ee1d1c2678e
chwfi <wondory11@naver.com>
2024-04-17 09:00:48 +0900

    평타 이펙트 개선
    평타 이펙트 개선

--- Assets/01.Scripts/Feedback/AttackEffectFeedback.cs
+++ Assets/01.Scripts/Feedback/AttackEffectFeedback.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-
 public class AttackEffectFeedback : Feedback
 {
     [SerializeField] private float _endTime;
@@ -14,14 +13,8 @@ public class AttackEffectFeedback : Feedback
             _calledNum = 1;
 
         EffectPlayer effect = PoolManager.Instance.Pop($"SlashEffect0{_calledNum}") as EffectPlayer;
-        effect.transform.position = ownerTrm.position;
-
-        if (_calledNum == 1)
-            effect.transform.rotation = 
-                Quaternion.Euler(new Vector3(ownerTrm.rotation.x, ownerTrm.rotation.y - 180f, ownerTrm.rotation.z - 150f));
-        else if (_calledNum == 2)
-            effect.transform.rotation =
-                Quaternion.Euler(new Vector3(ownerTrm.rotation.x, ownerTrm.rotation.y - 180f, ownerTrm.rotation.z));
+        effect.transform.position = transform.position;
+        effect.transform.rotation = transform.rotation;
         effect.StartPlay(_endTime);
     }
 
