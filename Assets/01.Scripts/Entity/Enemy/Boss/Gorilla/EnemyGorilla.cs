0cc8d0f3fcc64ef6451d4a929e07fe59418b6ed5
rlatjdgh1122 <98933564+rlatjdgh1122@users.noreply.github.com>
2024-04-17 19:05:51 +0900

    고릴라 제작 중

--- Assets/01.Scripts/Entity/Enemy/Boss/Gorilla/EnemyGorilla.cs
+++ Assets/01.Scripts/Entity/Enemy/Boss/Gorilla/EnemyGorilla.cs
@@ -1,7 +1,38 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyGorilla : Enemy
 {
+    public EnemyStateMachine<EnemyPenguinStateEnum> StateMachine { get; private set; }
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        StateMachine = new EnemyStateMachine<EnemyPenguinStateEnum>();
+
+        foreach (EnemyPenguinStateEnum state in Enum.GetValues(typeof(EnemyPenguinStateEnum)))
+        {
+            string typeName = state.ToString();
+            Type t = Type.GetType($"EnemyAnimal{typeName}State");
+            //리플렉션
+            var newState = Activator.CreateInstance(t, this, StateMachine, typeName) as EnemyState<EnemyPenguinStateEnum>;
+
+            StateMachine.AddState(state, newState);
+        }
+    }
+
+    protected override void Start()
+    {
+        StateMachine.Init(EnemyPenguinStateEnum.Idle);
+    }
+
+    protected override void Update()
+    {
+        StateMachine.CurrentState.UpdateState();
+    }
+
+    public override void AnimationTrigger() => StateMachine.CurrentState.AnimationFinishTrigger();
 }
