54fae2f1fe10d9a073276eb77e1d47d815b65eef
chwfi <wondory11@naver.com>
2024-04-21 00:44:32 +0900

    Activator.CreateInstance 사용하여 코드 변경
    리플렉션으로 리팩토링

--- Assets/01.Scripts/Entity/General/Katana/KatanaGeneralPenguin.cs
+++ Assets/01.Scripts/Entity/General/Katana/KatanaGeneralPenguin.cs
@@ -13,16 +13,31 @@ public class KatanaGeneralPenguin : General
         StateMachine = new PenguinStateMachine();
         Transform stateTrm = transform.Find("States");
 
+        //foreach (PenguinStateType state in Enum.GetValues(typeof(PenguinStateType)))
+        //{
+        //    IState stateScript = stateTrm.GetComponent($"Penguin{state}State") as IState;
+        //    if (stateScript == null)
+        //    {
+        //        Debug.LogError($"There is no script : {state}");
+        //        return;
+        //    }
+        //    stateScript.SetUp(this, StateMachine, state.ToString());
+        //    StateMachine.AddState(state, stateScript);
+        //}
+
         foreach (PenguinStateType state in Enum.GetValues(typeof(PenguinStateType)))
         {
-            IState stateScript = stateTrm.GetComponent($"Penguin{state}State") as IState;
-            if (stateScript == null)
+            string typeName = state.ToString();
+            Type t = Type.GetType($"Penguin{typeName}State");
+            //리플렉션
+            IState newState = Activator.CreateInstance(t) as IState;
+            if (newState == null)
             {
                 Debug.LogError($"There is no script : {state}");
                 return;
             }
-            stateScript.SetUp(this, StateMachine, state.ToString());
-            StateMachine.AddState(state, stateScript);
+            newState.SetUp(this, StateMachine, typeName);
+            StateMachine.AddState(state, newState);
         }
     }
 
