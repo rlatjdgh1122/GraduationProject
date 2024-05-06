0180220f15b17e04637df552ace68e3dd3efb53c
mingyo0125 <wjdalsry125@gmail.com>
2024-05-06 13:39:43 +0900

    .

---
+++ Assets/01.Scripts/Building/Buildings/DefenseBuilding/SignalTower/SignalTower.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SignalTower : BaseBuilding
+{
+    protected override void Running()
+    {
+    }
+}
