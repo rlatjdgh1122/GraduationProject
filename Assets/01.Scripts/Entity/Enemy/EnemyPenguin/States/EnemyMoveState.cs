286fba1b8154fca9c18d725cfd91754c440671b4
chwfi <wondory11@naver.com>
2024-05-06 15:31:19 +0900

    적이 Target 못 찾는 버그 수정
    적이 Target 못 찾는 버그 수정

--- Assets/01.Scripts/Entity/Enemy/EnemyPenguin/States/EnemyMoveState.cs
+++ Assets/01.Scripts/Entity/Enemy/EnemyPenguin/States/EnemyMoveState.cs
@@ -15,11 +15,10 @@ public class EnemyMoveState : EnemyBaseState
     {
         base.UpdateState();
 
-        if (_enemy.NexusTarget != null)
-            _enemy.MoveToNexus();
-
         if (_enemy.IsTargetInInnerRange)    
             _stateMachine.ChangeState(EnemyStateType.Chase); //가는 도중에 감지 사거리 내에 타겟 플레이어가 있으면 Chase로
+        else if (_enemy.NexusTarget != null)
+            _enemy.MoveToNexus();
 
         if (_enemy.IsReachedNexus)
             _stateMachine.ChangeState(EnemyStateType.Reached);
