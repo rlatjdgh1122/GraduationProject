ce46816d622208e7074242e968a4662237f37fbc
rlatjdgh1122 <98933564+rlatjdgh1122@users.noreply.github.com>
2024-05-09 19:34:05 +0900

    아처타워펭귄 ㅂ그 수정

--- Assets/01.Scripts/Building/Buildings/DefenseBuilding/ArcherTower/ArcherTowerPenguin/ArcherTowerPenguin.cs
+++ Assets/01.Scripts/Building/Buildings/DefenseBuilding/ArcherTower/ArcherTowerPenguin/ArcherTowerPenguin.cs
@@ -7,42 +7,41 @@ public enum ArcherTowerPenguinStateEnum
 {
     Idle,
     Attack,
-    Dead
 }
 
 
 public class ArcherTowerPenguin : Penguin  
 {
-    public EntityStateMachine <ArcherTowerPenguinStateEnum, Penguin> StateMachine { get; private set; }
+    public EntityStateMachine <ArcherTowerPenguinStateEnum, Penguin> _StateMachine { get; private set; }
 
     protected override void Awake()
     {
         base.Awake();
 
-        StateMachine = new EntityStateMachine<ArcherTowerPenguinStateEnum, Penguin>();
+        _StateMachine = new EntityStateMachine<ArcherTowerPenguinStateEnum, Penguin>();
 
         foreach (ArcherTowerPenguinStateEnum state in Enum.GetValues(typeof(ArcherTowerPenguinStateEnum)))
         {
             string typeName = state.ToString();
             Type t = Type.GetType($"ArcherTower{typeName}State");
             //리플렉션
-            var newState = Activator.CreateInstance(t, this, StateMachine, typeName) as EntityState<ArcherTowerPenguinStateEnum, Penguin>;
+            var newState = Activator.CreateInstance(t, this, _StateMachine, typeName) as EntityState<ArcherTowerPenguinStateEnum, Penguin>;
 
-            StateMachine.AddState(state, newState);
+            _StateMachine.AddState(state, newState);
         }
     }
 
     protected override void Start()
     {
-        StateMachine.Init(ArcherTowerPenguinStateEnum.Idle);
+        _StateMachine.Init(ArcherTowerPenguinStateEnum.Idle);
     }
 
     protected override void Update()
     {
-        StateMachine.CurrentState.UpdateState();
+        _StateMachine.CurrentState.UpdateState();
     }
 
-    public override void AnimationTrigger() => StateMachine.CurrentState.AnimationFinishTrigger();
+    public override void AnimationTrigger() => _StateMachine.CurrentState.AnimationFinishTrigger();
 
     protected override void HandleDie()
     {
--- Assets/01.Scripts/Building/Buildings/DefenseBuilding/ArcherTower/ArcherTowerPenguin/ArcherTowerPenguinStates/ArcherTowerBaseState.cs
+++ Assets/01.Scripts/Building/Buildings/DefenseBuilding/ArcherTower/ArcherTowerPenguin/ArcherTowerPenguinStates/ArcherTowerBaseState.cs
@@ -16,9 +16,6 @@ public class ArcherTowerBaseState : EntityState<ArcherTowerPenguinStateEnum, Pen
     public override void UpdateState()
     {
         base.UpdateState();
-
-        if (_penguin.IsDead)
-            _stateMachine.ChangeState(ArcherTowerPenguinStateEnum.Dead);
     }
 
     public override void Exit()
