7f96b01bd99b894d7306d607e167add825b6f198
Ojunsun <98933734+Ojunsun@users.noreply.github.com>
2024-05-13 21:26:20 +0900

    Ui 수정 및 댄스 추가
    Ui 수정 및 댄스 추가

--- Assets/01.Scripts/Entity/Enemy/EnemyPenguin/Bomb/States/EnemyBombPenguinAttackState.cs
+++ Assets/01.Scripts/Entity/Enemy/EnemyPenguin/Bomb/States/EnemyBombPenguinAttackState.cs
@@ -10,15 +10,6 @@ public class EnemyBombPenguinAttackState : EnemyBaseState
     public override void EnterState()
     {
         base.EnterState();
-
-        if (_enemy.UseAttackCombo)
-        {
-            AttackComboEnter();
-        }
-        else
-        {
-            AttackEnter();
-        }
     }
 
     public override void UpdateState()
@@ -26,41 +17,15 @@ public class EnemyBombPenguinAttackState : EnemyBaseState
         base.UpdateState();
 
         _enemy.LookTarget();
-
-        if (_triggerCalled) //공격이 한 차례 끝났을 때,
-        {
-            if (_enemy.IsTargetInInnerRange)
-                _stateMachine.ChangeState(EnemyStateType.Chase); //사거리 안에 타겟 플레이어가 있다 -> 따라가
-
-            else // 감지 사거리 내에서 벗어났다 -> Move (넥서스쪽으로)
-                _stateMachine.ChangeState(EnemyStateType.Move);
-        }
-
     }
 
     public override void ExitState()
     {
         base.ExitState();
-
-        if (_enemy.UseAttackCombo)
-        {
-            AttackComboExit();
-        }
-        else
-        {
-            AttackExit();
-        }
     }
 
     public override void AnimationTrigger()
     {
         base.AnimationTrigger();
-
-        if (_enemy.CheckAttackEventPassive(++curAttackCount))
-        {
-            _enemy.OnPassiveAttackEvent();
-
-            curAttackCount = 0;
-        }
     }
 }
