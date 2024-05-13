e855d2f5cb1ac1c76bed43b56f121ea92d771b1c
Ojunsun <98933734+Ojunsun@users.noreply.github.com>
2024-05-13 19:19:32 +0900

    폭탄탄 스태이트 제작 및 UI 낙사 수정
    폭탄탄 스태이트 제작 및 UI 낙사 수정

---
+++ Assets/01.Scripts/Entity/Enemy/EnemyPenguin/Bomb/States/EnemyBombPenguinChaseState.cs
@@ -0,0 +1,40 @@
+
+using UnityEngine;
+
+public class EnemyBombPenguinChaseState : EnemyBaseState
+{
+    public EnemyBombPenguinChaseState(Enemy enemy, EnemyStateMachine stateMachine, string animBoolName) : base(enemy, stateMachine, animBoolName)
+    {
+    }
+    public override void EnterState()
+    {
+        base.EnterState();
+
+        ChaseEnter();
+    }
+
+    public override void UpdateState()
+    {
+        base.UpdateState();
+
+        if (_enemy.CurrentTarget != null)
+        {
+            _enemy.MoveToCurrentTarget();
+        }
+
+        if (_enemy.IsTargetInInnerRange) //공격 사거리 내에 들어왔다 -> Attack
+        {
+            if (_enemy.IsTargetInAttackRange)
+                _stateMachine.ChangeState(EnemyStateType.Attack);
+        }
+        else // 감지 사거리 내에서 벗어났다 -> Move (넥서스쪽으로)
+            _stateMachine.ChangeState(EnemyStateType.Move);
+    }
+
+    public override void ExitState()
+    {
+        base.ExitState();
+
+        ChaseExit();
+    }
+}
---
+++ Assets/01.Scripts/Entity/Enemy/EnemyPenguin/Bomb/States/EnemyBombPenguinJumpState.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyBombPenguinJumpState : EnemyBaseState
+{
+    public EnemyBombPenguinJumpState(Enemy enemy, EnemyStateMachine stateMachine, string animBoolName) : base(enemy, stateMachine, animBoolName)
+    {
+    }
+
+    public override void EnterState()
+    {
+        base.EnterState();
+
+        _triggerCalled = false;
+        _enemy.MoveToNexus();
+    }
+
+    public override void UpdateState()
+    {
+        base.UpdateState();
+
+        if (_triggerCalled)
+            _stateMachine.ChangeState(EnemyStateType.Move);
+    }
+
+    public override void ExitState()
+    {
+        base.ExitState();
+    }
+}
