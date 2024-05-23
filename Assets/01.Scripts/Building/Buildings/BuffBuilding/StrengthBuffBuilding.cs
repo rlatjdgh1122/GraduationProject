5cb11d485deec039ea46aa539b179eb3594fe535
rlatjdgh1122 <98933564+rlatjdgh1122@users.noreply.github.com>
2024-05-23 09:48:41 +0900

    버프 건물 수정중

--- Assets/01.Scripts/Building/Buildings/BuffBuilding/StrengthBuffBuilding.cs
+++ Assets/01.Scripts/Building/Buildings/BuffBuilding/StrengthBuffBuilding.cs
@@ -1,10 +1,5 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.InputSystem;
-using UnityEngine.UIElements;
 
 public class StrengthBuffBuilding : BuffBuilding
 {
