f7f9246f231b46b1bea6c111b4d90317e105da41
rlatjdgh1122 <98933564+rlatjdgh1122@users.noreply.github.com>
2024-05-24 00:14:36 +0900

    스모

--- Assets/01.Scripts/Entity/Skill/ThrowSkill.cs
+++ Assets/01.Scripts/Entity/Skill/ThrowSkill.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 public class ThrowSkill : Skill
 {
     [SerializeField] private float _throwDelay;
+    [SerializeField] private float _throwValue = 2f;
+
+    private Transform _visualTrm = null;
+
+    private Vector3 _normal = Vector3.zero;
+
+    private Quaternion _cashingLocalRot = Quaternion.identity;
 
     public override void SetOwner(Entity owner)
     {
@@ -21,19 +28,33 @@ public class ThrowSkill : Skill
         {
             Grab();
             CanUseSkill = true;
-        } 
+        }
     }
 
-    public void Grab() 
+    public void Grab()
     {
-        _owner.CurrentTarget.transform.DORotate(new Vector3(0, 0, 90), 1.1f);
-        _owner.CurrentTarget.transform.DOMoveY(2.8f, 1.2f).SetEase(Ease.OutQuint);
+        _visualTrm = _owner.CurrentTarget.transform.Find("Visual");
+
+        /* RaycastHit raycastHit;
+         bool raycastSuccess = Physics.Raycast(transform.position, transform.forward, out raycastHit, 10, _owner.TargetLayer);
+         if (raycastSuccess)
+         {
+             _normal = raycastHit.normal;
+         }
+ */
+        _cashingLocalRot = _visualTrm.localRotation;
+
+        _visualTrm.DORotate(new Vector3(90, 0, 0), 1.1f);
+        _visualTrm.DOMoveY(2.8f, 1.2f).SetEase(Ease.OutQuint);
     }
 
     public void Throw()
     {
         if (!_owner.IsTargetInThrowRange) return;
 
-        _owner.CurrentTarget.HealthCompo.Knockback(4, -transform.forward);
+        _visualTrm.localRotation = _cashingLocalRot;
+        _visualTrm.localPosition = Vector3.zero;
+
+        _owner.CurrentTarget.HealthCompo.Knockback(_throwValue, transform.forward);
     }
 }
