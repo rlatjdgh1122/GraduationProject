30a7da6b073a8bb077ba8beb8727dab636fc88f5
leewnstj <ij35492581@gmail.com>
2024-06-26 01:43:20 +0900

    추가 ChangeVolume
    게임의 포스트 프로세싱을 관리하는 코드

---
+++ Assets/01.Scripts/Effect/ChangeVolume.cs
@@ -0,0 +1,99 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public enum VolumeType
+{
+    None = 0,
+    LunarEclipse,
+    Test1,
+    Test2,
+}
+
+//나중에 타입 종류가 많아지면 SO로 빼자
+[Serializable]
+public class TypeOfVolumeProfile
+{
+    public VolumeType Type;
+    public Volume Volume;
+    public float ChangingDuration;
+}
+
+public class ChangeVolume : MonoBehaviour
+{
+    [SerializeField]
+    private List<TypeOfVolumeProfile> _volumeProfileList = new List<TypeOfVolumeProfile>();
+
+    private Dictionary<VolumeType, Coroutine> _activeCoroutines = new Dictionary<VolumeType, Coroutine>();
+
+#if UNITY_EDITOR
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Y))
+        {
+            Debug.Log("디버그용입니당나중에지워주세용");
+            ChangeVolumeEffect(VolumeType.LunarEclipse, 3f);
+        }
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            Debug.Log("디버그용입니당나중에지워주세용");
+            ChangeVolumeEffect(VolumeType.Test1, 3f, true);
+        }
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            Debug.Log("디버그용입니당나중에지워주세용");
+            ChangeVolumeEffect(VolumeType.Test2, 5f);
+        }
+    }
+#endif
+
+    public void ChangeVolumeEffect(VolumeType volumeType, float duration, bool loop = false)
+    {
+        var profile = _volumeProfileList.FirstOrDefault(p => p.Type == volumeType);
+
+        if (profile != null && profile.Volume != null)
+        {
+            // 이전에 실행 중인 코루틴이 있으면 중지
+            if (_activeCoroutines.ContainsKey(volumeType) && _activeCoroutines[volumeType] != null)
+            {
+                StopCoroutine(_activeCoroutines[volumeType]);
+            }
+
+            // 새로운 코루틴을 시작하고 Dictionary에 저장
+            _activeCoroutines[volumeType] = StartCoroutine(ChangeVolumeRoutine(profile.Volume, profile.ChangingDuration, duration, loop));
+        }
+        else
+        {
+            Debug.LogError($"VolumeProfile of {volumeType} is NULL or not found!");
+        }
+    }
+
+    private IEnumerator ChangeVolumeRoutine(Volume volume, float changeDuration, float duration, bool loop)
+    {
+        yield return LerpVolumeWeight(volume, 0, 1, changeDuration);
+
+        if (!loop)
+        {
+            yield return new WaitForSeconds(duration);
+            yield return LerpVolumeWeight(volume, 1, 0, changeDuration);
+        }
+    }
+
+    private IEnumerator LerpVolumeWeight(Volume volume, float startWeight, float endWeight, float duration)
+    {
+        float elapsed = 0f;
+
+        while (elapsed < duration)
+        {
+            elapsed += Time.deltaTime;
+            volume.weight = Mathf.Lerp(startWeight, endWeight, elapsed / duration);
+            yield return null;
+        }
+
+        volume.weight = endWeight;
+    }
+}
