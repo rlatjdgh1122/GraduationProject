21379d4231b1f5a54e3bcd239153b5493d36e9a9
mingyo0125 <wjdalsry125@gmail.com>
2024-06-29 20:52:41 +0900

    수정 카타나장군 Volumes

--- Assets/01.Scripts/Entity/Skill/Ultimate/Katana/KatanaUltimate.cs
+++ Assets/01.Scripts/Entity/Skill/Ultimate/Katana/KatanaUltimate.cs
@@ -7,13 +7,18 @@ public class KatanaUltimate : Skill
 {
     public UnityEvent OnStartEclipseEvent;
 
+    private ChangeVolume _changeVolume;
+
     public override void SetOwner(Entity owner)
     {
         base.SetOwner(owner);
+
+        _changeVolume = FindAnyObjectByType<ChangeVolume>();
     }
 
     public override void PlaySkill()
     {
         OnStartEclipseEvent?.Invoke();
+        _changeVolume.ChangeVolumeEffect(VolumeType.LunarEclipse, 3f);
     }
 }
