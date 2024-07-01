be63623bea2cee7dfaf39753b2a96610549602b9
mingyo0125 <wjdalsry125@gmail.com>
2024-07-01 18:23:23 +0900

    추가 선택된 장군만 스킬을 사용하도록

---
+++ Assets/01.Scripts/Entity/Skill/Katana/KatanaUltimate.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class KatanaUltimate : Skill
+{
+    public UnityEvent OnStartEclipseEvent;
+
+    private ChangeVolume _changeVolume;
+
+    public override void SetOwner(Entity owner)
+    {
+        base.SetOwner(owner);
+
+        _changeVolume = FindAnyObjectByType<ChangeVolume>(); // 스레기같은 방법이니까 나중에 바꿔야됨
+    }
+
+    public override void PlaySkill()
+    {
+        OnStartEclipseEvent?.Invoke();
+        _changeVolume.ChangeVolumeEffect(VolumeType.LunarEclipse, 3f);
+    }
+}
---
+++ Assets/01.Scripts/Entity/Skill/Ultimate/Lance/LanceUltSkill.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LanceUltSkill : MonoBehaviour
+{
+    LancePenguinUltAttackableEntity _lanceUltEntity;
+
+    private void Awake()
+    {
+        _lanceUltEntity = transform.parent.GetComponent<LancePenguinUltAttackableEntity>();
+    }
+
+    public void LanceUltimate()
+    {
+        Debug.Log("찌이이이이르으으으기");
+        _lanceUltEntity.TruckAttack(LookMouse());
+    }
+
+    private Vector3 LookMouse()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, new Vector3(0, transform.position.y, 0)); // 객체의 높이에서 평면 설정
+
+        if (groundPlane.Raycast(ray, out float distance))
+        {
+            return ray.GetPoint(distance); // 마우스의 월드 위치 반환
+        }
+        return Vector3.zero; // 평면을 교차하지 않는 경우
+    }
+}
