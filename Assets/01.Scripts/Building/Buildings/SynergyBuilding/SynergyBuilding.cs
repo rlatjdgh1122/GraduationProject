6a07ba860ab3a9292eab0753a1f4c67013d67878
chwfi <wondory11@naver.com>
2024-07-02 19:18:53 +0900

    시너지 건물 UI 기능과 스탯
    시너지 건물 UI 기능과 스탯

--- Assets/01.Scripts/Building/Buildings/SynergyBuilding/SynergyBuilding.cs
+++ Assets/01.Scripts/Building/Buildings/SynergyBuilding/SynergyBuilding.cs
@@ -4,8 +4,32 @@ using UnityEngine;
 
 public class SynergyBuilding : BaseBuilding
 {
+    private BuildingUI _buildingPanel;
+    private Health _health;
+
+    protected override void Awake()
+    {
+        _buildingPanel = UIManager.Instance.canvasTrm.GetComponentInChildren<BuildingUI>();
+        _health = GetComponent<Health>();
+
+        _health.SetHealth(_characterStat);
+    }
+
     protected override void Running()
     {
 
+    }
+
+    private void OnMouseDown()
+    {
+        if (!WaveManager.Instance.IsBattlePhase)
+        {
+            UIManager.Instance.ShowPanel("BuildingUI");
+            _buildingPanel.BuildingStat = (SynergyBuildingStat)_characterStat;
+            _buildingPanel.BuildingHealth = _health;
+
+            _buildingPanel.SetStat();
+            SignalHub.OnDefaultBuilingClickEvent?.Invoke();
+        }
     }
 }
