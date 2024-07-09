99de42c065fcb205f6aa7e73f60de2387689a35c
chwfi <wondory11@naver.com>
2024-07-09 14:33:18 +0900

    슬롯 추가 구매 Panel 제작
    슬롯 추가 구매 Panel 제작

---
+++ Assets/01.Scripts/UI/NormalCanvasUI/ArmyUI/Legion/PlusSlotPanel.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlusSlotPanel : PopupUI
+{
+    private Button _purchaseButton;
+    private LegionSlotPurchaseButton _slot;
+
+    public override void Awake()
+    {
+        base.Awake();
+
+        _purchaseButton = transform.Find("Popup/PurchaseButton").GetComponent<Button>();
+        _purchaseButton.onClick.AddListener(OnPurchase);
+    }
+
+    public override void ShowPanel()
+    {
+        base.ShowPanel();
+    }
+
+    private void OnPurchase()
+    {
+        _slot.Purchase();
+        HidePanel();
+    }
+
+    public void Setting(LegionSlotPurchaseButton slot)
+    {
+        _slot = slot;
+    }
+
+    public override void HidePanel()
+    {
+        base.HidePanel();
+    }
+}
