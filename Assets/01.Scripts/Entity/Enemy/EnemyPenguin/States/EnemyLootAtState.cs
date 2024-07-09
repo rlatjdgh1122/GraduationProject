2caf40e411f6188ec86af81fb81760b0a0a91f02
rlatjdgh1122 <98933564+rlatjdgh1122@users.noreply.github.com>
2024-07-09 11:01:07 +0900

    lookatstate만드는중

---
+++ Assets/01.Scripts/Entity/Enemy/EnemyPenguin/States/EnemyLootAtState.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyLootAtState : State
+{
+    public EnemyLootAtState(Penguin penguin, PenguinStateMachine stateMachine, string animationBoolName) : base(penguin, stateMachine, animationBoolName)
+    {
+    }
+
+    public override void EnterState()
+    {
+        base.EnterState();
+    }
+  
+    public override void UpdateState()
+    {
+        base.UpdateState();
+    }
+
+    public override void ExitState()
+    {
+        base.ExitState();
+    }
+
+}
