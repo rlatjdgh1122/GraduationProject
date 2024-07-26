5f90595efdf11f0027e7bb2e0feaf10231fc36e5
mingyo0125 <wjdalsry125@gmail.com>
2024-07-26 15:58:20 +0900

    수정 멀어져도 사운드 들리도록

--- Assets/01.Scripts/0.Core/Manager/SoundManager.cs
+++ Assets/01.Scripts/0.Core/Manager/SoundManager.cs
@@ -131,7 +131,7 @@ public class SoundManager : MonoBehaviour
     /// <param name="type">SFX이니? BGM이니?</param>
     /// <param name="rolloffMode">이건 그냥 건들지 마셈</param>
     public static void Play3DSound(SoundName clipName, Vector3 position,
-    float minFOV = 20, float maxFOV = 55, float maxDistance = 80,
+    float minFOV = 20, float maxFOV = 60, float maxDistance = 55,
     SoundType type = SoundType.SFX,
     AudioRolloffMode rolloffMode = AudioRolloffMode.Linear)
     {
@@ -214,6 +214,7 @@ public class SoundManager : MonoBehaviour
         source.rolloffMode = rolloffMode;
 
         float fovNormalization = 1 - (float)(Cam.MainCam.fieldOfView - minFOV) / (maxFOV - minFOV);
+        fovNormalization = Mathf.Clamp(fovNormalization, 0.1f, 1);
         source.volume = fovNormalization;
 
         source.outputAudioMixerGroup = type switch
