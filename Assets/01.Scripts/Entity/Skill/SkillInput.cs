5be1f7031b0bd1307efc0b5f961eb6a6cc5361c0
rlatjdgh1122 <rlatjdgh2358@naver.com>
2024-07-28 20:15:26 +0900

    장군 예외처리

--- Assets/01.Scripts/Entity/Skill/SkillInput.cs
+++ Assets/01.Scripts/Entity/Skill/SkillInput.cs
@@ -1,54 +1,63 @@
+using ArmySystem;
+using System;
 using UnityEngine;
 
 public class SkillInput : MonoBehaviour
 {
     [SerializeField] InputReader _inputReader;
 
-    private General _curSelectGeneral = null;
+    private Army _selectArmy = null;
 
     private bool CanPlaySkill()
     {
-        return _curSelectGeneral != null; /* && 디버깅 하기 위해 주석 처리함. 나중에는 주석 풀 것.
+        return _selectArmy.General != null; /* && 디버깅 하기 위해 주석 처리함. 나중에는 주석 풀 것.
                WaveManager.Instance.IsBattlePhase; */
     }
 
     private void OnEnable()
     {
+        SignalHub.OnArmyChanged += OnArmyChangedEventHandler;
         _inputReader.OnSkillEvent += OnPlaySkill;
         _inputReader.OnUltimateEvent += OnPlayUltimateSkill;
+    }
+
+    private void OnArmyChangedEventHandler(Army prevArmy, Army newArmy)
+    {
+        _selectArmy = newArmy;
     }
 
     private void OnPlaySkill()
     {
         if (!CanPlaySkill()) { return; }
 
-        if (_curSelectGeneral.Skill.SkillController.CheckDecision())
+        if (_selectArmy.General.Skill.SkillController.CheckDecision())
         {
-            _curSelectGeneral.OnSkillEvent();
+            _selectArmy.General.OnSkillEvent();
 
-            _curSelectGeneral.Skill.SkillController.OnUsed();
+            _selectArmy.General.Skill.SkillController.OnUsed();
         }
     }
 
     private void OnPlayUltimateSkill()
     {
-        if (!CanPlaySkill() || !_curSelectGeneral.IsSynergy) { return; }
-        Debug.Log(_curSelectGeneral.Ultimate.SkillController.CheckDecision());
-        if (_curSelectGeneral.Ultimate.SkillController.CheckDecision())
+        if (!CanPlaySkill() || !_selectArmy.General.IsSynergy) { return; }
+        //Debug.Log(_curSelectGeneral.Ultimate.SkillController.CheckDecision());
+        if (_selectArmy.General.Ultimate.SkillController.CheckDecision())
         {
-            _curSelectGeneral.OnUltimateEvent();
+            _selectArmy.General.OnUltimateEvent();
 
-            _curSelectGeneral.Ultimate.SkillController.OnUsed();
+            _selectArmy.General.Ultimate.SkillController.OnUsed();
         }
     }
 
     public void SelectGeneral(General general) //ArmyManager의 OnChangedArmy에서 선택된 General을 받음
     {
-        _curSelectGeneral = general;
+        //_curSelectGeneral = general;
     }
 
     private void OnDisable()
     {
+        SignalHub.OnArmyChanged -= OnArmyChangedEventHandler;
         _inputReader.OnSkillEvent -= OnPlaySkill;
         _inputReader.OnUltimateEvent -= OnPlayUltimateSkill;
     }
