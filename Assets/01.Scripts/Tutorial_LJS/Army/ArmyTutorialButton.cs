214d73da0d57b862a71e7760310b7fd4483bad03
leewnstj <ij35492581@gmail.com>
2024-07-28 20:05:13 +0900

    일단 튜토리얼 기능만
    일단 튜토리얼 기능만

--- Assets/01.Scripts/Tutorial_LJS/Army/ArmyTutorialButton.cs
+++ Assets/01.Scripts/Tutorial_LJS/Army/ArmyTutorialButton.cs
@@ -1,15 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class ArmyTutorialButton : MonoBehaviour
 {
     [SerializeField] private TutorialInfoUI _tutorialUI;
     [SerializeField] private ArmyTutorial _armyTutorial;
 
-    private List<GameObject> _selectArmyList = new();
-    private List<GameObject> _getGeneralList = new();
-    private List<GameObject> _insetGeneralInArmyList = new();
+    public UnityEvent _getGeneralEvent = new();
+    public UnityEvent _endTutorialEvent = new();
 
     public void SelectArmyButton()
     {
@@ -19,12 +19,19 @@ public class ArmyTutorialButton : MonoBehaviour
     public void GetGeneralButton()
     {
         _tutorialUI.CompleteSlot(_armyTutorial.CurrentTutorial(1));
+
+        _getGeneralEvent?.Invoke();
     }
 
     public void InsetGeneralInArmyButton()
     {
         _tutorialUI.CompleteSlot(_armyTutorial.CurrentTutorial(2));
+        _endTutorialEvent?.Invoke();
 
         UIManager.Instance.HidePanel("ArmyUI");
+        UIManager.Instance.ResetPanel();
+
+        TutorialController.Instance.AddTutorialIndex();
+
     }
 }
