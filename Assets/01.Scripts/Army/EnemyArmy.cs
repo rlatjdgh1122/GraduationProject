2cd8dd8c56a8ac1a5b9e872120420a04d3b2d999
rlatjdgh1122 <98933564+rlatjdgh1122@users.noreply.github.com>
2024-10-13 15:50:16 +0900

    버그 수정
    다일 타겟 중일때도 해당 타겟의 군단 안에 있는 펭귄을 타겟할 수 있게 그리고 단일타겟은 아웃라인 다르게

--- Assets/01.Scripts/Army/EnemyArmy.cs
+++ Assets/01.Scripts/Army/EnemyArmy.cs
@@ -5,171 +5,164 @@ using UnityEngine;
 
 namespace ArmySystem
 {
-    [System.Serializable]
-    public class EnemyArmy
-    {
-        public EnemyArmy(List<Enemy> soldiers)
-        {
-            _soldiers = soldiers;
+	[System.Serializable]
+	public class EnemyArmy
+	{
+		public EnemyArmy(List<Enemy> soldiers)
+		{
+			_soldiers = soldiers.ToList();
 
-            //처음 군단 설정할 때도 타겟 정해줌
-            TargetSoliders = soldiers.ToList();
+			//처음 군단 설정할 때도 타겟 정해줌
+			TargetSoliders = soldiers.ToList();
 
-            //군단 설정
-            _soldiers.ForEach(enemy => enemy.JoinEnemyArmy(this));
+			//군단 설정
+			_soldiers.ForEach(enemy => enemy.JoinEnemyArmy(this));
 
-        }
+		}
 
-        //얘가 null이라면 군단 선택이 안된 것과 같음 
-        private Enemy _singleTarget = null;
+		//얘가 null이라면 군단 선택이 안된 것과 같음 
+		private Enemy _singleTarget = null;
 
-        //실제 군단에 포함되어 있는 모든 펭귄들을 담고 있음
-        public List<Enemy> _soldiers = new();
+		//실제 군단에 포함되어 있는 모든 펭귄들을 담고 있음
+		public List<Enemy> _soldiers = new();
 
-        //실제로 타겟을 지정할 땐 이 리스트를 사용하여 지정
-        public List<Enemy> TargetSoliders = new();
+		//실제로 타겟을 지정할 땐 이 리스트를 사용하여 지정
+		public List<Enemy> TargetSoliders = new();
 
-        public int SoliderCount => _soldiers.Count;
-        public bool IsSelected = false;
+		public int SoliderCount => _soldiers.Count;
+		public bool IsSelected = false;
 
-        private Color _mouseOverColor = Color.white;
-        private Color _selectColor = Color.white;
+		private Color _mouseOverColor = Color.white;
+		private Color _selectColor = Color.white;
 
-        private bool IsNull => _soldiers == null;
+		private bool IsNull => _soldiers == null;
 
-        public void OnChangedOutlineColorHandler(Color overColor, Color selectColor)
-        {
-            _mouseOverColor = overColor;
-            _selectColor = selectColor;
-        }
+		public void OnChangedOutlineColorHandler(Color overColor, Color selectColor)
+		{
+			_mouseOverColor = overColor;
+			_selectColor = selectColor;
+		}
 
-        public void RemoveEnemy(Enemy enemy)
-        {
-            _soldiers.Remove(enemy);
+		public void RemoveEnemy(Enemy enemy)
+		{
+			_soldiers.Remove(enemy);
+			TargetSoliders.Remove(enemy);
 
-            if (_singleTarget == null && _singleTarget.Equals(enemy))
-            {
-                _singleTarget = null;
+			enemy.OutlineCompo.enabled = false;
 
-            } //end if
+			if (SoliderCount <= 0)
+			{
+				EnemyArmyManager.Instance.DeleteArmy(this);
 
-            enemy.OutlineCompo.enabled = false;
+			} //end if
 
-            if (SoliderCount <= 0)
-            {
-                EnemyArmyManager.Instance.DeleteArmy(this);
+		}
 
-            } //end if
+		public void SetSingleTarget(Enemy enemy)
+		{
+			_singleTarget = enemy;
+		}
 
-        }
+		public void SetSingleTargetMode(bool isSingleTargetMode)
+		{
+			//타겟을 지정하기전 아웃라인을 다 지운 후
+			DeSelectedOutline();
 
-        public void SetSingleTarget(Enemy enemy)
-        {
-            _singleTarget = enemy;
-        }
+			TargetSoliders.Clear();
+			if (isSingleTargetMode)
+			{
+				//단일 타겟지정
+				TargetSoliders.Add(_singleTarget);
 
-        public void SetSingleTargetMode(bool isSingleTargetMode)
-        {
-            //타겟을 지정하기전 아웃라인을 다 지운 후
-            DeSelectedOutline();
+			} //end if
+			else
+			{
+				//군단 타겟지정, 값복사하기
+				TargetSoliders = _soldiers.ToList();
 
-            if (isSingleTargetMode)
-            {
-                TargetSoliders.Clear();
+			} //end else
 
-                //단일 타겟지정
-                TargetSoliders.Add(_singleTarget);
+			//타겟이 지정되었다면 다시 아웃라인을 켜줌
+			OnSelectedOutline();
+		}
 
-            } //end if
-            else
-            {
-                TargetSoliders.Clear();
+		#region MouseEvent
 
-                //군단 타겟지정, 값복사하기
-                TargetSoliders = _soldiers.ToList();
+		public void OnMouseEnter()
+		{
+			if (IsSelected) return;
 
-            } //end else
+			SetSingleTargetMode(EnemyArmyManager.Instance.IsSingleTargetMode);
 
-            //타겟이 지정되었다면 다시 아웃라인을 켜줌
-            OnSelectedOutline();
-        }
+			foreach (Enemy enemy in TargetSoliders)
+			{
+				enemy.OutlineCompo.enabled = true;
+				if (enemy.OutlineCompo.isActiveAndEnabled)
+				{
+					enemy.OutlineCompo.SetColor(_mouseOverColor);
+				}
+			}
+		}
 
-        #region MouseEvent
+		public void OnMouseExit()
+		{
+			if (IsSelected)
+			{
+				OnSelectedOutline();
+			}
+			else
+			{
+				DeSelectedOutline();
 
-        public void OnMouseEnter()
-        {
-            if (IsSelected) return;
+			}//end else
 
-            SetSingleTargetMode(EnemyArmyManager.Instance.IsSingleTargetMode);
+		}
 
-            foreach (Enemy enemy in TargetSoliders)
-            {
-                enemy.OutlineCompo.enabled = true;
-                if (enemy.OutlineCompo.isActiveAndEnabled)
-                {
-                    enemy.OutlineCompo.SetColor(_mouseOverColor);
-                }
-            }
-        }
+		public void OnClick()
+		{
+			EnemyArmyManager.Instance.OnSelected(this);
+		}
 
-        public void OnMouseExit()
-        {
-            if (IsSelected)
-            {
-                OnSelectedOutline();
-            }
-            else
-            {
-                DeSelectedOutline();
+		#endregion
 
-            }//end else
+		public void OnSelected()
+		{
+			IsSelected = true;
 
-        }
+			OnSelectedOutline();
+		}
 
-        public void OnClick()
-        {
-            EnemyArmyManager.Instance.OnSelected(this);
-        }
+		public void DeSelected()
+		{
+			IsSelected = false;
 
-        #endregion
+			DeSelectedOutline();
+		}
 
-        public void OnSelected()
-        {
-            IsSelected = true;
-            OnSelectedOutline();
-        }
+		private void OnSelectedOutline()
+		{
+			foreach (Enemy enemy in TargetSoliders)
+			{
+				enemy.OutlineCompo.enabled = true;
+				if (enemy.OutlineCompo != null && enemy.OutlineCompo.isActiveAndEnabled)
+				{
+					enemy.OutlineCompo.SetColor(_selectColor);
+				}
+			}//end foreach
+		}
 
-        public void DeSelected()
-        {
-            IsSelected = false;
+		private void DeSelectedOutline()
+		{
+			foreach (Enemy enemy in TargetSoliders)
+			{
+				if (enemy.OutlineCompo != null && enemy.OutlineCompo.isActiveAndEnabled)
+				{
+					enemy.OutlineCompo.enabled = false;
+				}
+			}//end foreach
+		}
 
-            DeSelectedOutline();
-        }
-
-        private void OnSelectedOutline()
-        {
-            foreach (Enemy enemy in TargetSoliders)
-            {
-                enemy.OutlineCompo.enabled = true;
-                if (enemy.OutlineCompo != null && enemy.OutlineCompo.isActiveAndEnabled)
-                {
-                    enemy.OutlineCompo.SetColor(_selectColor);
-                }
-            }//end foreach
-        }
-
-        private void DeSelectedOutline()
-        {
-            foreach (Enemy enemy in TargetSoliders)
-            {
-                if (enemy.OutlineCompo != null && enemy.OutlineCompo.isActiveAndEnabled)
-                {
-                    enemy.OutlineCompo.enabled = false;
-                }
-            }//end foreach
-        }
-
-    }
+	}
 
 
 }
