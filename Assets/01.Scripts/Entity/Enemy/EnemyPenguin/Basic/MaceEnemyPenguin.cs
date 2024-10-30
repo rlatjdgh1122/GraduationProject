30afcd95d69692134a61190514021f0eb3e3a054
rlatjdgh1122 <98933564+rlatjdgh1122@users.noreply.github.com>
2024-10-30 15:33:34 +0900

    적 장군

--- Assets/01.Scripts/Entity/Enemy/EnemyPenguin/Basic/MaceEnemyPenguin.cs
+++ Assets/01.Scripts/Entity/Enemy/EnemyPenguin/Basic/MaceEnemyPenguin.cs
@@ -4,13 +4,109 @@ using UnityEngine;
 
 public class MaceEnemyPenguin : EnemyBasicPenguin
 {
-    protected override void Start() 
+    public List<BuffItem> BuffList = new();
+    public float HealingDelay = 10f;
+
+    [System.Serializable]
+    public class BuffItem
+    {
+        public int Wave;
+        public string BuffName;
+        public int BuffValue;
+    }
+
+    private Coroutine _healCorou = null;
+    private float _defaultAttackSpeed = 0f;
+
+    private BuffItem _curBuffItem = null;
+
+    protected override void Start()
     {
         base.Start();
 
-        foreach (var enemy in MyArmy.Soldiers)
-        {
-            enemy.StartEffect("Health");
-        }    
+        _curBuffItem = BuffList.Find(x => WaveManager.Instance.CurrentWaveCount == x.Wave);
+
+        GiveBuff(_curBuffItem);
     }
+
+    protected override void HandleDie()
+    {
+        base.HandleDie();
+
+        TakeBuff(_curBuffItem);
+    }
+
+    private void GiveBuff(BuffItem item)
+    {
+        switch (item.BuffName)
+        {
+            case "Health":
+                foreach (var solider in MyArmy.Soldiers)
+                {
+                    solider.StartEffect(item.BuffName);
+                    _healCorou = StartCoroutine(Heal(item.BuffValue, solider));
+                }
+                break;
+
+            case "AttackSpeed":
+                foreach (var solider in MyArmy.Soldiers)
+                {
+                    solider.StartEffect(item.BuffName);
+                    _defaultAttackSpeed = solider.attackSpeed;
+                    solider.attackSpeed = item.BuffValue;
+                }
+                break;
+
+            case "Damage":
+                foreach (var solider in MyArmy.Soldiers)
+                {
+                    solider.StartEffect(item.BuffName);
+                    solider.AddStat(item.BuffValue, StatType.Damage, StatMode.Increase);
+                }
+                break;
+        }
+    }
+
+    private void TakeBuff(BuffItem item)
+    {
+        switch (item.BuffName)
+        {
+            case "Health":
+                foreach (var solider in MyArmy.Soldiers)
+                {
+                    solider.StopEffect(item.BuffName);
+                    StopCoroutine(_healCorou);
+                }
+                break;
+
+            case "AttackSpeed":
+                foreach (var solider in MyArmy.Soldiers)
+                {
+                    solider.StopEffect(item.BuffName);
+                    solider.attackSpeed = _defaultAttackSpeed;
+                }
+                break;
+
+            case "Damage":
+                foreach (var solider in MyArmy.Soldiers)
+                {
+                    solider.StopEffect(item.BuffName);
+                    solider.RemoveStat(item.BuffValue, StatType.Damage, StatMode.Increase);
+                }
+                break;
+
+        }
+    }
+
+    private IEnumerator Heal(int value, Enemy target)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(HealingDelay);
+            target.HealthCompo.ApplyHeal(value);
+        }
+
+    }
+
+
 }
